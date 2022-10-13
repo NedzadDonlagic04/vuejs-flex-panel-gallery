aa93d44c10128bd89f524777357581c2d5148668
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-10-14 00:29:58 +0200

    Added component to represent the panel

---
+++ src/components/FlexImage.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+    props: {
+        path: {
+            type: String,
+            required: true
+        },
+        flexGrow: {
+            type: Number,
+            required: true
+        }
+    },
+    computed: {
+        imgSrc() 
+        {
+            return new URL(this.path, import.meta.url);
+        }
+    },
+    emits: ['imageExpand'],
+    methods: {
+        clickHandle()
+        {
+            this.$emit('imageExpand', this.$.vnode.key);
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="img-box" ref="div" :style="{ flexGrow: flexGrow, backgroundImage: `url(${imgSrc})`}" @click="clickHandle"></div>
+</template>
+
+<style scoped>
+    .img-box {
+        min-height: 100%;
+        transition: all .5s ease;
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center;
+    }
+
+    .img-box:hover {
+        cursor: pointer;
+    }
+</style>
