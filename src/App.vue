182f5f9efd88990c1cf76eaa85fc5e157cce76d3
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-10-14 00:30:25 +0200

    Implemented the FlexImage component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,53 @@
+<script>
+import FlexImage from './components/FlexImage.vue';
+
+let id = 0;
+
+export default {
+    components: {
+        FlexImage
+    },
+    data()
+    {
+        return {
+            images: [
+                { id: id++, path: './../assets/foggy-lake.jpg', flexGrow: 1 },
+                { id: id++, path: './../assets/fox.jpeg', flexGrow: 1 },
+                { id: id++, path: './../assets/hike.jpeg', flexGrow: 1 },
+                { id: id++, path: './../assets/sky.jpeg', flexGrow: 1 },
+                { id: id++, path: './../assets/winter-mountain.jpeg', flexGrow: 1 }
+            ]
+        }
+    },
+    methods: {
+        expandHandle(value)
+        {
+            for(let i=0; i<this.images.length; i++)
+            {
+                if(this.images[i].id === value)
+                {
+                    this.images[i].flexGrow = 4;
+                }
+                else
+                {
+                    this.images[i].flexGrow = 1;
+                }
+            }
+        }
+    }
+}
+</script>
+
 <template>
-    <div>
-    g    <p>Hello World!</p>
+    <div class="container">
+        <FlexImage v-for="image in images" :key="image.id" :path="image.path" :flexGrow="image.flexGrow" @imageExpand="expandHandle"/>
     </div>
 </template>
+
+<style scoped>
+    .container {
+        min-height: 100vh;
+        display: flex;
+        flex-direction: row;
+    }
+</style>
