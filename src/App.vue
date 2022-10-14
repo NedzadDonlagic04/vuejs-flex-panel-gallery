5913265b8120cf392846a47827eb532f58cea4cf
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-10-14 20:03:34 +0200

    Applied the new text attributes

--- src/App.vue
+++ src/App.vue
@@ -7,31 +7,25 @@ export default {
     components: {
         FlexImage
     },
-    data()
+    data() 
     {
         return {
             images: [
-                { id: id++, path: './../assets/foggy-lake.jpg', flexGrow: 1 },
-                { id: id++, path: './../assets/fox.jpeg', flexGrow: 1 },
-                { id: id++, path: './../assets/hike.jpeg', flexGrow: 1 },
-                { id: id++, path: './../assets/sky.jpeg', flexGrow: 1 },
-                { id: id++, path: './../assets/winter-mountain.jpeg', flexGrow: 1 }
+                { id: id++, path: './../assets/foggy-lake.jpg', flexGrow: 1, topText: "hey", middleText: "let's", bottomText: "dance" },
+                { id: id++, path: './../assets/fox.jpeg', flexGrow: 1, topText: "give", middleText: "take", bottomText: "receive" },
+                { id: id++, path: './../assets/hike.jpeg', flexGrow: 1, topText: "experience", middleText: "it", bottomText: "today" },
+                { id: id++, path: './../assets/sky.jpeg', flexGrow: 1, topText: "give", middleText: "all", bottomText: "you can" },
+                { id: id++, path: './../assets/winter-mountain.jpeg', flexGrow: 1, topText: "life", middleText: "in", bottomText: "motion" }
             ]
         }
     },
     methods: {
         expandHandle(value)
         {
-            for(let i=0; i<this.images.length; i++)
+            for(let i=0; i < this.images.length; i++)
             {
-                if(this.images[i].id === value)
-                {
-                    this.images[i].flexGrow = 4;
-                }
-                else
-                {
-                    this.images[i].flexGrow = 1;
-                }
+                const image = this.images[i];
+                image.flexGrow = (image.id === value) ? 4 : 1;
             }
         }
     }
@@ -40,7 +34,14 @@ export default {
 
 <template>
     <div class="container">
-        <FlexImage v-for="image in images" :key="image.id" :path="image.path" :flexGrow="image.flexGrow" @imageExpand="expandHandle"/>
+        <FlexImage v-for="image in images" 
+            :key="image.id" 
+            :path="image.path" 
+            :flexGrow="image.flexGrow" 
+            :topText="image.topText"
+            :middleText="image.middleText"
+            :bottomText="image.bottomText"
+            @imageExpand="expandHandle"/>
     </div>
 </template>
 
