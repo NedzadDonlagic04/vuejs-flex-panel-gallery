bf932103971f55947fd069fddc0139a9c594a4ee
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-10-14 20:03:20 +0200

    Added top, middle and bottom text

--- src/components/FlexImage.vue
+++ src/components/FlexImage.vue
@@ -8,6 +8,18 @@ export default {
         flexGrow: {
             type: Number,
             required: true
+        },
+        topText: {
+            type: String,
+            required: true
+        },
+        middleText: {
+            type: String,
+            required: true
+        },
+        bottomText: {
+            type: String,
+            required: true
         }
     },
     computed: {
@@ -27,7 +39,11 @@ export default {
 </script>
 
 <template>
-    <div class="img-box" ref="div" :style="{ flexGrow: flexGrow, backgroundImage: `url(${imgSrc})`}" @click="clickHandle"></div>
+    <div class="img-box" ref="div" :style="{ flexGrow: flexGrow, backgroundImage: `url(${imgSrc})`}" @click="clickHandle">
+        <p class="top-text">{{topText}}</p>
+        <p class="middle-text">{{middleText}}</p>
+        <p class="bottom-text">{{bottomText}}</p>
+    </div>
 </template>
 
 <style scoped>
@@ -37,9 +53,38 @@ export default {
         background-size: cover;
         background-repeat: no-repeat;
         background-position: center;
+        position: relative;
     }
 
     .img-box:hover {
         cursor: pointer;
+    }
+
+    p {
+        text-transform: uppercase;
+        color: white;
+        position: absolute;
+        text-shadow: -2px 0px 10px black;
+    }
+
+    .top-text {
+        font-size: 3rem;
+        top: 15%;
+        left: 50%;
+        transform: translate(-50%, -15%);
+    }
+
+    .middle-text {
+        font-size: 4rem;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+
+    .bottom-text {
+        font-size: 3rem;
+        bottom: 15%;
+        left: 50%;
+        transform: translate(-50%);
     }
 </style>
